1a79e4126f42e28a0eb2f34c9fbff9a61eea7008
Barayuda Gautama <saya.bara@gmail.com>
2019-07-06 04:17:50 +0800

    Fix about page display on mobile

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -5,10 +5,10 @@
         <div class="container">
           <div class="columns is-vcentered">
             <div class="column is-3 is-offset-1 landing-caption">
-              <h1 class="title is-3 has-text-weight-light has-text-left is-spaced">
+              <h1 class="title is-3 has-text-weight-light has-text-left has-text-centered-mobile is-spaced">
                 {{ title }}
               </h1>
-              <p class="subtitle is-6 has-text-left is-muted">
+              <p class="subtitle is-6 has-text-left is-muted is-hidden-mobile">
                 {{ description }}
               </p>
             </div>
@@ -16,6 +16,9 @@
               <figure class="image is-4by3">
                 <img src="../assets/images/rainy@3x.png" alt="Description" />
               </figure>
+              <p class="subtitle is-6 has-text-centered-mobile m-t-xl is-muted is-hidden-desktop is-hidden-tablet">
+                {{ description }}
+              </p>
             </div>
           </div>
         </div>
